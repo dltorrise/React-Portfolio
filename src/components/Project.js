bf59958cab58a1b5588735950e0f4ac8f6ad8ea5
dltorrise <dltorrise@gmail.com>
2023-04-28 10:40:05 -0500

    formatting finishes

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,13 +5,13 @@ function Project({project}) {
         <div>
             <section className="column">
                 <figure className="card-code">
-                    <h2 className="title text-center text-white">{project.name}</h2>
+                    <h2 className="title text-left text-white">{project.name}</h2>
                     <a href={project.deployedLink}>
                         <img className="border border-white border-2" style={{width:"100vw", height:"auto"}} src={project.img} alt={project.alt} />
                     </a>
                     <p className="text-center mt-1 text-white">Description: {project.description} Click on image to view more!</p>
                     <p className="text-center text-white">Skills: {project.skills}</p>
-                    <a className="text-center text-white" href={project.githubLink}><p>GitHub Repo</p></a>
+                    <a className="text-center text-white" href={project.githubLink}><p>GitHub Repo 😸</p></a>
                 </figure>
             </section>
         </div>
--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -6,7 +6,7 @@ export default function AboutMe() {
     <div>
       <h1 className="text-center my-3 text-white">About Me</h1>
       <figure>
-      <img id="headshot" style={{width:"100vw", height:"auto"}} src={headshot} alt="A headshot of me smiling at Harold Washington Library"/>
+      <img id="headshot" style={{width:"100vw", maxHeight:"350px", objectFit:"cover"}} src={headshot} alt="A headshot of me smiling at Harold Washington Library"/>
     </figure>
       <p id="aboutme" className="text-center text-white">
       My name is Danielle Torrisè (Ta-Ree-See). I am a Chicago native looking to work full-time as a Web Developer! Check out some of my favorite projects in the Projects section ☺
