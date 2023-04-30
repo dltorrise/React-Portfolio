12c05cc23bfbea87be70b9d7332921440930d265
dltorrise <dltorrise@gmail.com>
2023-04-30 17:00:49 -0500

    dropbox link

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,8 +4,8 @@ export default function Resume() {
   return (
     <div>
       <h1 className="text-center mb-3 text-white">Resume</h1>
-      <a href="/assets/resume/resume.pdf"><p className=" mb-3 text-center text-white">
-        Download PDF
+      <a href="https://www.dropbox.com/s/w0ok9crwfudaogq/Danielle%20Torrise%20LinkedIn%20Resume%202023.pdf?dl=0"><p className=" mb-3 text-center text-white">
+        Download Link
       </p></a>
     </div>
   );
